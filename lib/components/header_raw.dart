59e673339b15016c5d547f85ef13c67cfebca319
Shruti Baviskar <shrutibaviskar0808@gmail.com>
2026-02-16 16:14:44 +0530

    Add dynamic time-based greetings to header

--- lib/components/header_raw.dart
+++ lib/components/header_raw.dart
@@ -15,6 +15,17 @@ class HeaderRaw extends StatelessWidget {
     this.isMenuVisible = true,
   });
 
+  String getGreeting() {
+    final hour = DateTime.now().hour;
+    if (hour >= 6 && hour < 12) {
+      return "Good Morning !";
+    } else if (hour >= 12 && hour < 18) {
+      return "Good Afternoon !";
+    } else {
+      return "Good Evening !";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -45,7 +56,7 @@ class HeaderRaw extends StatelessWidget {
                   ),
                 ),
                 Text(
-                  "Good Morning !",
+                  getGreeting(),
                   style: titleTextStyle.copyWith(
                     color: Colors.white70,
                     fontSize: 12,
